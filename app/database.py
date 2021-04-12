2c040d3708f844e7166b47516a6dd15bd426c6ab
cnanda2 <cnanda2@illinois.edu>
2021-04-12 04:20:18 -0500

    finished Apartment CRUD

--- app/database.py
+++ app/database.py
@@ -130,12 +130,74 @@ def fetchUser(username):
         return False    
     return True
 
+
+def duplicate_address(address):
+    cur = db.connect()
+    query = f" SELECT Address FROM Apartments WHERE Address = '{address}'"
+    results = cur.execute(query)
+
+    arr = []
+    for row in results:
+        arr.append(row)
+    
+    if len(arr) < 1:
+        return False    
+    return True
+
+def insertApartment(array):
+    cur = db.connect()
+    for i in range(len(array)):
+        if i == 2 or i == 3 or i == 7:
+            if array[i] == "":
+                array[i] = 0
+            else:
+                try:
+                    array[i] = int(array[i])
+                except:
+                    return False
+
+        if array[i] == "":
+            array[i] = "N/A"
+    
+        
+    query = f"INSERT INTO Apartments(Address, Complex_Name, Units, Stories, Building_Type, Street, Company, Rent) VALUES('{array[0]}', '{array[1]}', {array[2]}, {array[3]}, '{array[4]}', '{array[5]}', '{array[6]}', {array[7]})"
+    cur.execute(query)
+    return True
+
 def searchApartment(address):
     cur = db.connect()
-    query = f"SELECT * FROM apartments WHERE Address = '{address}'"
+    query = f"SELECT * FROM Apartments WHERE Address = '{address}'"
     results = cur.execute(query)
     ans = []
     for row in results:
         ans.append(row)
+    return ans
 
-    return ans[0]
+def updateApartment(field, update, address):
+    cur = db.connect()
+    try:
+        for i in range(len(field)):
+            query = f"UPDATE Apartments SET {field[i]} = '{update[i]}' WHERE Address = '{address}'"
+            results = cur.execute(query)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def deleteApartment(address):
+    cur = db.connect()
+    query = f"DELETE FROM Apartments WHERE Address = '{address}'"
+    try:
+        cur.execute(query)
+    except:
+        return False
+    return True
+
+def advQuery():
+    query = "SELECT A.Address, A.Rent, B.Company, D.Complex_Name FROM (SELECT avg(Rent) as average from Apartments) as C, Apartments A NATURAL JOIN Amenities B Natural JOIN Included_Utilities D WHERE B.Gym = True AND A.Rent < C.average AND D.maintainance = True order by Rent ASC LIMIT 15;"
+    cur = db.connect()
+    results = cur.execute(query)
+    ans = []
+    for row in results:
+        ans.append(row)
+    return ans
--- app/routes.py
+++ app/routes.py
@@ -8,21 +8,59 @@ import flask
 def apartment_create():
     text = request.form['text']
     parsed = text.split(",")
-    if(len(parsed) != 8):
-        result = {'success': 0}
-    else:
+    
+    if(db_helper.duplicate_address(parsed[0])):
+        result = {'success': 0, 'error': "address exists"}
+
+    elif(len(parsed) != 8):
+        result = {'success': 0, 'error': "incorrect length"}
+       
+    elif(db_helper.insertApartment(parsed)):
         result = {'success': 1}
+        
+    else:
+        result = {'success': 0, 'error': "int failure"}  
+
     return jsonify(result)
 
 
 @app.route("/apartment_read", methods=['POST'])
 def apartment_read():
     text = request.form['text']
+    print(text)
     search = db_helper.searchApartment(text)
     result = {'search' : search}
     return jsonify(result)
 
+@app.route("/apartment_update", methods=['POST'])
+def apartment_update():
+    field = request.form['field']
+    update = request.form['update']
+    address = request.form['address']
+    field = field.split(",")
+    update = update.split(",")
+    if(db_helper.updateApartment(field, update, address)):
+        result = {'success': 1}
+    else:
+        result = {'success': 0}
+    return jsonify(result)
 
+@app.route("/apartment_delete", methods=['POST'])
+def apartment_delete():
+    text = request.form['text']
+    if(not db_helper.duplicate_address(text)):
+        result = {'success': 0}
+        return jsonify(result)
+    if(db_helper.deleteApartment(text)):
+        result = {'success': 1}
+    else:
+        result = {'success': 0}
+    return jsonify(result)
+
+@app.route("/apartment_adv", methods=['POST'])
+def apartment_adv():
+    result = db_helper.advQuery()
+    return jsonify(result)
 
 
 
@@ -36,6 +74,7 @@ def create():
         return render_template("homepage.html")
     return render_template("Error.html")
 
+
 @app.route("/login", methods=['POST'])
 def login():
     username = request.form['username']
@@ -45,6 +84,7 @@ def login():
             return flask.redirect("/homepage")
 
     return flask.redirect("/Error")
+
 
 @app.route("/Error")
 def error_page():
